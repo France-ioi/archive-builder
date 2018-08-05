bb1a43986e238839eb1139f25015ab256dfae2ce
Sebastien Carlier <s.carlier@epixode.fr>
2018-08-05 23:35:10 +0000

    new version with worker queue and non-blocking interface

--- README.md
+++ README.md
@@ -3,21 +3,37 @@
 These variables must be defined in `.env` at the project root:
 
     PORT
+    SECRET
     S3_ACCESS_KEY_ID
     S3_SECRET_ACCESS_KEY
     S3_REGION
     S3_BUCKET
+
+SECRET should be a random value, for example generated with:
+
+```javascript
+node -e 'console.log(require("crypto").randomBytes(32).toString("base64"))'
+```
 
 Build and run with:
 
     yarn install
     npm start
 
-To generate a zip file, send a GET /?task=_TASK\_URL_ request to the
-webservice.  _TASK_URL_ should respond with JSON data describing the
-contents of the archive.
+To generate a zip file, navigate the user to path /?t=_TOKEN_, where
+_TOKEN_ is a JsonWebToken signed with the value of SECRET and containing
+a `manifestUrl` property.
 
-Example:
+The service will redirect to an HTML page that auto-refreshes.
+
+When the zip is ready, a download link will appear.
+
+## Manifests
+
+A manifest is JSON data describing the contents to include in an archive.
+
+The following manifest describes an archive incorporating the contents
+of `shared.zip` plus `index.html` and `data.json`.
 
 ```json
 {
@@ -38,7 +54,3 @@ Example:
   ]
 }
 ```
-
-This description will produce an archive incorporating the contents of
-`shared.zip` plus `index.html` and `data.json`.
-
--- index.js
+++ index.js
@@ -1,194 +1,120 @@
 
 require('dotenv').config();
-const http = require('http');
+
 const path = require('path');
-const tmp = require('tmp');
-const util = require('util');
-const stream = require('stream');
-const pfs = require('nano-fs');
-const fs = require('fs');
 const crypto = require('crypto');
-
-const pify = require('pify');
+const http = require('http');
 const express = require('express');
-const bodyParser = require('body-parser');
-const readdir = require('recursive-readdir');
-const mkdirp = pify(require('mkdirp'));
-const JSZip = require('jszip');
+const asyncHandler = require('express-async-handler');
+const doAsync = require('doasync');
+const jwt = doAsync(require('jsonwebtoken'));
 const aws = require('aws-sdk');
-const str = require('string-to-stream');
-const fetch = require('fetch');
-const base64 = require('base64-stream');
-const unzip = require('unzip');
+const Datastore = require('nedb');
+const Queue = require('better-queue');
 
-const pipeline = util.promisify(stream.pipeline);
+const worker = require('./src/worker');
+
+function P (f) {
+  return new Promise(function (resolve, reject) {
+    try {
+      f(function (err, result) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
 
 const config = {
   isDevelopment: process.env.NODE_ENV !== 'production',
   port: process.env.PORT || '8000'
 };
+const jobs = new Datastore({
+  filename: path.join(__dirname, 'jobs.db'),
+  autoload: true
+});
 const s3 = new aws.S3({
   accessKeyId: process.env.S3_ACCESS_KEY_ID,
   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
   region: process.env.S3_REGION
 });
 
-function fetchUrl (url) {
-  return new Promise (function (resolve, reject) {
-    fetch.fetchUrl(url, function (error, meta, body) {
-      if (error) return reject(error);
-      return resolve({meta, body});
-    });
+function sha256 (text) {
+  return crypto.createHash('sha256')
+    .update(text.toString())
+    .digest('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '');
+}
+
+const jobQueue = new Queue(function ({manifestUrl}, cb) {
+  worker(manifestUrl, {
+    s3: s3,
+    s3Bucket: process.env.S3_BUCKET,
+    s3BucketIsNew: process.env.S3_BUCKET_IS_NEW === '1'
+  }).then(function (url) {
+    cb(null, {url});
+  }, function (err) {
+    cb(err);
   });
-}
-
-function hashFile (filepath) {
-  return new Promise (function (resolve, reject) {
-    const hash = crypto.createHash('sha256');
-    const input = fs.createReadStream(filepath);
-    input.on('readable', () => {
-      const data = input.read();
-      if (data) {
-        hash.update(data);
-      } else {
-        resolve(hash.digest('hex'));
-      }
-    });
-    input.on('error', reject);
-  });
-}
-
-function makeSourceStream (source) {
-  let stream;
-  if (source.hasOwnProperty('url')) {
-    stream = new fetch.FetchStream(source.url);
-  } else if (source.hasOwnProperty('string')) {
-    stream = str(source.content);
-  } else if (source.hasOwnProperty('file')) {
-    // XXX append target dir if relative path
-    // XXX disallow absolute path?
-    stream = fs.createReadStream(source.file);
-  }
-  if (source.decode === 'base64') {
-    stream = stream.pipe(base64.decode());
-  }
-  return stream;
-}
-
-async function makeTargetStream (buildContext, target) {
-  let stream;
-  if (target.hasOwnProperty('unzip')) {
-    const targetPath = await buildContext.addFolder(target.unzip);
-    stream = unzip.Extract({path: targetPath});
-  } else if (target.hasOwnProperty('file')) {
-    stream = await buildContext.addFile(target.file);
-  }
-  if (target.encode === 'base64') {
-    stream = stream.pipe(base64.encode());
-  }
-  return stream;
-}
-
-class BuildContext {
-  constructor (req, res) {
-    this._req = req;
-    this._res = res;
-    this._cleanupCallbacks = [];
-    this._zip = new JSZip();
-  }
-  cleanup () {
-    for (let cb of this._cleanupCallbacks) {
-      cb();
-    }
-  }
-  addCleanupCallback (cb) {
-    this._cleanupCallbacks.unshift(cb);
-  }
-  async run (query) {
-    const taskResponse = await fetchUrl(query.task);
-    const spec = JSON.parse(taskResponse.body);
-    await this.makeTargetDir();
-    for (let insn of spec.contents) {
-      const inStream = makeSourceStream(insn.from);
-      const outStream = await makeTargetStream(this, insn.to);
-      await pipeline(inStream, outStream);
-    }
-    await this.populateZip();
-    await this.generateZip();
-    const hash = await hashFile(this.targetZipPath);
-    const body = await pfs.readFile(this.targetZipPath);
-    const bucket = process.env.S3_BUCKET;
-    const key = `${hash}.zip`;
-    await s3.putObject({
-      Bucket: bucket,
-      Key: key,
-      ACL: 'public-read',
-      ContentType: 'application/zip',
-      Body: body
-    }).promise();
-    return `https://s3.amazonaws.com/${bucket}/${key}`;
-  }
-  makeTargetDir () {
-    return new Promise ((resolve, reject) => {
-      tmp.dir({unsafeCleanup: true}, (err, path, cleanupCallback) => {
-        if (err) return reject(err);
-        this.addCleanupCallback(cleanupCallback);
-        this.targetDir = path;
-        resolve();
-      });
-    });
-  }
-  makeTargetZip () {
-    return new Promise ((resolve, reject) => {
-      tmp.file({postfix: '.zip'}, (err, path, fd, cleanupCallback) => {
-        if (err) return reject(err);
-        this.addCleanupCallback(cleanupCallback);
-        this.targetZipPath = path;
-        const zipStream = fs.createWriteStream(null, {fd: fd, encoding: 'binary'});
-        resolve(zipStream);
-      });
-    });
-  }
-  async populateZip () {
-    const files = await readdir(this.targetDir);
-    const prefixLen = this.targetDir.length + 1;
-    for (let file of files) {
-      const relPath = file.substring(prefixLen);
-      const content = await pfs.readFile(file);
-      this._zip.file(relPath, content);
-    }
-  }
-  async generateZip () {
-    const inStream = this._zip.generateNodeStream({compression: 'DEFLATE'});
-    const outStream = await this.makeTargetZip();
-    await pipeline(inStream, outStream);
-  }
-  async addFolder (relPath) {
-    const absPath = path.join(this.targetDir, relPath);
-    await mkdirp(absPath);
-    return absPath;
-  }
-  async addFile (filename) {
-    const absPath = path.join(this.targetDir, filename);
-    await mkdirp(path.dirname(absPath));
-    return fs.createWriteStream(absPath);
-  }
-}
+}, {
+  maxTimeout: 120 * 1000 /* milliseconds */
+});
+jobQueue.on('task_accepted', function (taskId, task) {
+  jobs.update({taskKey: task.taskKey}, {$set: {status: 'accepted', taskId}});
+});
+jobQueue.on('task_queued', function (taskId, task) {
+  jobs.update({taskId}, {$set: {status: 'queued'}});
+});
+jobQueue.on('task_started', function (taskId, task) {
+  jobs.update({taskId}, {$set: {status: 'started'}});
+});
+jobQueue.on('task_finish', function (taskId, result, stats) {
+  jobs.update({taskId}, {$set: {status: 'finished', result, stats}});
+});
+jobQueue.on('task_failed', function (taskId, err, stats) {
+  jobs.update({taskId}, {$set: {status: 'failed', error: err, stats}});
+});
+jobQueue.on('task_progress', function (taskId, progress) {
+  jobs.update({taskId}, {$set: {progress}});
+});
+jobQueue.on('task_retry', function (taskId, retries) {
+  jobs.update({taskId}, {$set: {retries}});
+});
 
 const app = express();
 app.enable('strict routing');
-app.use(bodyParser.json());
-app.get('/', function (req, res) {
-  const context = new BuildContext(req, res);
-  context.run(req.query).then(function (url) {
-    context.cleanup();
-    res.redirect(url);
-  }, function (err) {
-    context.cleanup();
-    res.status(500).send(err.stack);
-  });
-});
-const server = http.createServer(app);
-console.info(`Starting on port ${config.port}`)
-server.listen(config.port);
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
+app.get('/', asyncHandler(async (req, res) => {
+  const token = await jwt.verify(req.query.t, process.env.SECRET, {audience: 'builder'});
+  const manifestUrl = token.manifestUrl;
+  const taskKey = sha256(manifestUrl);
+  let job = await P(cb => jobs.findOne({taskKey}, cb));
+  if (!job) {
+    await P(cb => jobs.insert({taskKey, manifestUrl}, cb));
+    job = {taskKey, manifestUrl};
+    jobQueue.push(job);
+  }
+  res.redirect(`/jobs/${job.taskKey}`);
+}));
+
+app.get('/jobs/:taskKey', asyncHandler(async (req, res) => {
+  const taskKey = req.params.taskKey;
+  const job = await P(cb => jobs.findOne({taskKey}, cb));
+  if (job.status != 'finished') {
+    res.header('Refresh', 5);
+  }
+  res.render('job', {job});
+}));
+
+console.info(`Starting on port ${config.port}`)
+const server = http.createServer(app);
+server.listen(config.port);
